159f8a454db863d2d60d8bf720cffcf6167bb244
sean kelly <skelly@munsterbovine.ie>
2026-01-06 00:11:04 +0000

    Theme addition

--- composeApp/src/commonMain/kotlin/com/example/sharpwave2026/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/sharpwave2026/App.kt
@@ -1,9 +1,12 @@
 package com.example.sharpwave2026
 
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import com.example.sharpwave2026.ui.SharpWaveApp
+import com.example.sharpwave2026.ui.theme.ArcWaveTheme
 
 @Composable
 fun App() {
-    SharpWaveApp()
+    ArcWaveTheme {
+        SharpWaveApp()
+    }
 }
