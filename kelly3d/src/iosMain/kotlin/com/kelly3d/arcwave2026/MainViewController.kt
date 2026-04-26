3b9dd531c8b3846337c04736effe819043726e93
sean kelly <skelly@munsterbovine.ie>
2026-04-26 09:47:53 +0100

    Person updates

--- kelly3d/src/iosMain/kotlin/com/kelly3d/arcwave2026/MainViewController.kt
+++ kelly3d/src/iosMain/kotlin/com/kelly3d/arcwave2026/MainViewController.kt
@@ -1,6 +1,5 @@
 package com.kelly3d.arcwave2026
 
 import androidx.compose.ui.window.ComposeUIViewController
-import com.kelly3d.arcwave2026.App
 
 fun MainViewController() = ComposeUIViewController { App() }
