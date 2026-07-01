ddfabcea3efe5bd846b28aae9b44691b2b7973dc
sean kelly <skelly@munsterbovine.ie>
2026-07-01 22:04:48 +0100

    Adding junit setup

--- kelly3d/build.gradle.kts
+++ kelly3d/build.gradle.kts
@@ -70,6 +70,11 @@ android {
             isMinifyEnabled = false
         }
     }
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
